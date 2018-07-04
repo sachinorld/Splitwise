288a2ce345240080e0b8e15a79c239d3ae63b10c
sachin <sachinorld@gmail.com>
2018-07-04 11:13:37 +0530

    Update SplitwiseMap.java

--- SplitwiseMap.java
+++ SplitwiseMap.java
@@ -5,48 +5,65 @@ import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class SplitwiseMap {
-
-	static HashMap<String, Double> param = new HashMap<String, Double>();
+static HashMap<String, Double> inMap = new HashMap<>();
 	
 	public static void main(String[] args) {
-		
-		getInput(param);
-		
-		
-		// divide between actors and subtract 1 for paying actor.
-		HashMap<String, Double> givenMap = new HashMap<>();
-		HashMap<String, Double> receiveMap = new HashMap<>();
-		for (Entry<String, Double> originalEntry : param.entrySet()) {
-			givenMap.put(originalEntry.getKey(), 0.0);
-			receiveMap.put(originalEntry.getKey(), 0.0);
+		getInput(inMap);
+		findPath(inMap);
+	}
+
+	private static void findPath(HashMap<String, Double> inMap2) {
+		Double total = 0.0;
+//		inMap2.values().stream()inMap2.
+		for (Entry<String, Double> entry : inMap2.entrySet()) {
+			total += entry.getValue();
 		}
+		int size = inMap2.size();
+		Double avg = total / size;
 		
-		int num = param.size();
-		for (Entry<String, Double> entry : param.entrySet()) {
-			System.out.println(entry.getKey() + "" + entry.getValue());
-			
-			if (entry.getValue() != 0) {
-				double value = entry.getValue() / num;
-				givenMap.put(entry.getKey(), (num-1) * value); // amt to be received.
-				
-				for (Entry<String, Double> receiveEntry : param.entrySet()) {
-					if (!receiveEntry.getKey().equals(entry.getKey())) {
-						receiveMap.put(receiveEntry.getKey(), receiveMap.get(receiveEntry.getKey()) + value);
-					}
-				}
+		HashMap<String, Double> outMap = new HashMap<>();
+		outMap.putAll(inMap2);
+		
+		for (Entry<String, Double> entry : inMap2.entrySet()) {
+			if (!entry.getValue().equals(avg)) {
+				outMap.put(entry.getKey(), -1 * (avg - entry.getValue()));
 			} else {
-				
+				outMap.put(entry.getKey(), 0.0);
 			}
 		}
-		for (Entry<String, Double> divEntry : param.entrySet()) {
-			Double val = givenMap.get(divEntry.getKey()) - receiveMap.get(divEntry.getKey());
-			System.out.println(divEntry.getKey() + " - " + val);
+		System.out.println(outMap);
+		
+		output(outMap);
+	}
+
+	private static void output(HashMap<String, Double> outMap) {
+		Double max = Collections.max(outMap.values());
+		Double min = Collections.min(outMap.values());
+		if (max != 0.0 || min != 0.0) {
+			String max_key = getKeyForValue(outMap, max);
+			String min_key = getKeyForValue(outMap, min);
+			Double result = max + min;
+			if (result >= 0.0) {
+				System.out.println(min_key + " owes " + max_key + " " + Math.abs(min));
+				outMap.put(max_key, result);
+				outMap.put(min_key, 0.0);
+			} else {
+				System.out.println(min_key + " owes " + max_key + " " + Math.abs(max));
+				outMap.put(min_key, result);
+				outMap.put(max_key, 0.0);
+			} 
+			output(outMap);
 		}
 	}
 
-	/**
-	 * @param sc
-	 */
+	private static String getKeyForValue(HashMap<String, Double> details, Double value) {
+		for (String key : details.keySet()) {
+			if (details.get(key) == (value))
+				return key;
+		}
+		return null;
+	}
+
 	private static HashMap<String, Double> getInput(HashMap<String, Double> param) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter member name. ** Enter DONE when done.");
